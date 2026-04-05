c633b31f7dfe41f84c5ec8c439f8376d7328504b
Sidshot <sidshot@github.com>
2026-04-05 15:23:26 +0530

    Revert to static mode, avoid SSR crashes with Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import mdx from '@astrojs/mdx';
@@ -13,8 +12,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://median-point.vercel.app',
-  output: 'server',
-  adapter: vercel(),
   integrations: [mdx(), sitemap(), react(), keystatic()],
 
   vite: {
